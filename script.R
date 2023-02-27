6de08fd574fb7a7113779218daa8f65f201e45e8
Shreya Chatterjee <120051088+Statshreya@users.noreply.github.com>
2023-02-28 00:30:49 +0530

    Update script.R

--- script.R
+++ script.R
@@ -1,31 +1,23 @@
-rm(list=ls()) #removes all variable stored previously
+rm(list=ls()) 
 install.packages("Hmisc")
-library(Hmisc) #import
+library(Hmisc) 
 
 data <- read.csv("C:/Users/manoj/Downloads/Covid_R/COVID19_line_list_data.csv")
-describe(data) #Hmisc command
+describe(data) 
 
-#cleaned up death column
+
 data$death_dummy <- as.integer(data$death !=0)
 unique(data$death_dummy)
-# death rate
 sum(data$death_dummy / nrow(data))
-
-#AGE 
-#claim: people who die are older
 dead = subset(data, death_dummy == 1)
 alive = subset(data, death_dummy == 0)
 mean(dead$age, na.rm = TRUE)
 mean(alive$age, na.rm = TRUE)
 #is this statistically significant?
-
 t.test(alive$age, dead$age, alternative = "greater", conf.level = 0.95)
 #normally, if p-value > 0.05, we fail to reject the null hypothesis
 #here, p-value = 1, so we fail to reject the null hypothesis and 
 #conclude that this is statistically significant
-
-#Gender
-#claim: gender has no effect
 men = subset(data, gender == "male")
 women = subset(data, gender == "female")
 mean(men$death_dummy, na.rm = TRUE) # 8.5%
